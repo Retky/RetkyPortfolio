c4d1e21c7d5501cbd68a40bac7635d959947c89f
Retky <jose.arredondo.a@outlook.com>
2022-05-26 15:10:15 -0500

    Add 'About' base section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Landing from './components/Landing';
+import About from './components/About';
 import './App.scss';
 
 const App = () => (
   <div>
     <Nav />
     <Landing />
+    <About />
   </div>
 );
 
