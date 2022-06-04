0e04e2ef506c4ac0dc30810bb34a17d78fa28b13
Retky <jose.arredondo.a@outlook.com>
2022-06-04 13:21:43 -0500

    Modify 'form' action
    To send to formspree

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,11 +8,11 @@ const Contact = () => (
         I&apos;m always interested in hearing about new projects,
         so if you&apos;d like to chat please get in touch.
       </h3>
-      <form id="contactForm">
+      <form id="contactForm" action="https://formspree.io/f/mqknqqzb" method="post">
         <input type="text" name="name" placeholder="Full Name" required />
         <input type="email" name="mail" placeholder="Email Address" required />
         <textarea name="message" placeholder="Enter Text Here" maxLength="500" required />
-        <input id="contactBtn" type="submit" name="contact" value="Get in touch" />
+        <input id="contactBtn" type="submit" value="Get in touch" />
       </form>
     </div>
   </section>
