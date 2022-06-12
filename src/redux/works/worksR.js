d8a12f84b82e56314379cff287eff6c751ee5cd4
Retky <jose.arredondo.a@outlook.com>
2022-06-12 11:34:31 -0500

    Modify 'store' to use mock project

--- src/redux/works/worksR.js
+++ src/redux/works/worksR.js
@@ -1,4 +1,20 @@
-const initialState = [{ id: 1, name: 'Testing' }];
+const initialState = [{
+  id: 1,
+  source: '/',
+
+  name: 'Testing',
+  description: 'lorem ipsum',
+  topics: ['HTML', 'CSS', 'Bootstrap', 'ruby'],
+  homepage: '/',
+
+  preview: '/',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+}];
+
+// GET DESCRIPTION
+// https://api.github.com/repos/Retky/Mv-Retky-Portfolio
+// GET AN IMAGAE
+// https://raw.githubusercontent.com/Retky/Mv-Retky-Portfolio/main/img/AboutIllustration.png
 
 const reducer = (state = initialState) => (state);
 
