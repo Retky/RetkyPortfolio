9b5f14174eca86d6f96f9ec0f79d7c7636401fac
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-09-19 11:55:40 -0500

    Add 'mobile menu' base style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import './Nav.scss';
 
-const Nav = () => (
-  <header id="header">
-    <div id="navBar">
-      <a className="navText" href="#landing"><div id="logo">Retky</div></a>
-      <nav id="navlist">
+const Nav = () => {
+  const hideNav = { display: 'none' };
+  return (
+    <header id="header">
+      <div id="navBar">
+        <a className="navText" href="#landing"><div id="logo">Retky</div></a>
+        <nav id="navlist">
+          <li><a href="#works">Projects</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </nav>
+        <div id="burger">=</div>
+      </div>
+      <div id="burgerMenu" style={hideNav}>
         <li><a href="#works">Projects</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
-      </nav>
-      <div id="burger">=</div>
-    </div>
-  </header>
-);
+      </div>
+    </header>
+  );
+};
 
 export default Nav;
