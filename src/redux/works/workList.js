cbcf80a0546a7b41a89bee14340389f7c8d33616
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2023-05-03 18:40:35 -0600

    Mod 'workList' to fetch from a database

--- src/redux/works/workList.js
+++ src/redux/works/workList.js
@@ -1,8 +1,16 @@
-const worksList = [
-  'CreativeCourses-Page',
-  'SpaceTravelers',
-  'RentACar',
-  'BudgetApp',
-];
+const fetchworkList = async () => {
+  const response = await fetch('https://portfolio-rtk-default-rtdb.firebaseio.com/Projects.json', {
+    method: 'GET',
+  });
+  const data = await response.json();
+  return data;
+};
+
+const worksList = async () => {
+  const data = await fetchworkList();
+  const works = [];
+  Object.keys(data).forEach((key) => { works.push(data[key]); });
+  return works;
+};
 
 export default worksList;
