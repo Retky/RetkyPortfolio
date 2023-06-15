d4c0b03b40dada94c663507c8407955a8df9caf9
J. Luis Arredondo <retky@thefoxsign.com>
2023-06-15 13:58:10 -0600

    Add 'Landing content'

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,24 @@
+import '../styles/HomePage.scss';
+
 const Homepage = () => {
   const view = (
-    <div className="home">
-      <h1>Home Page</h1>
-    </div>
+    <main className="home">
+      <div className="landing">
+        <div className="landing-content">
+          <h1>
+            Hello,
+            <br />
+            I&apos;m Luis Arredondo
+            <br />
+            <span>Full-Stack Web & Video Game Developer.</span>
+          </h1>
+          <p>
+            Are you looking to create captivating experiences and solve
+            complex problems through the power of code? I can help you with that.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 
   return view;
