59eb7d175a540c83e4185c514e039fa0cb5cab0f
J. Luis Arredondo <retky@thefoxsign.com>
2023-06-21 07:43:58 -0600

    Add 'aboutMe' section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -33,9 +33,54 @@ const Homepage = () => {
       </div>
       {/* <div className="projects">
       </div> */}
-      {/* <div className="about">
-        a
-      </div> */}
+      <div className="about">
+        <div className="about-content">
+          <h2>About Me</h2>
+          <p>
+            a
+          </p>
+          <button type="button">Get My Resume</button>
+        </div>
+        <div className="stack">
+          <ul>
+            <h3>Languages</h3>
+            <li>JavaScript</li>
+            <li>Ruby</li>
+            <li>C#</li>
+            <li>PostgreSQL</li>
+            <li>SQL</li>
+            <li>Lua</li>
+            <li>HTML</li>
+            <li>CSS</li>
+          </ul>
+          <ul>
+            <h3>Libraries & Frameworks</h3>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Node.js</li>
+            <li>Ruby on Rails</li>
+            <li>SCSS</li>
+            <li>Bootstrap</li>
+            <li>Webpack</li>
+            <li>Jest</li>
+            <li>Rspec</li>
+          </ul>
+          <ul>
+            <h3>Tools</h3>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>Firebase</li>
+            <li>Postman</li>
+            <li>Heroku</li>
+            <li>Netlify</li>
+            <li>Unity</li>
+            <li>Unreal Engine</li>
+            <li>Blender</li>
+            <li>3DS Max</li>
+            <li>Figma</li>
+          </ul>
+        </div>
+      </div>
       {/* <div className="contact">
       </div> */}
     </main>
