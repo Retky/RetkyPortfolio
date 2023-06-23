5f386cae202a8d6e57254f293cca48124dc99c99
J. Luis Arredondo <retky@thefoxsign.com>
2023-06-23 10:53:03 -0600

    Add 'contact form' section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -85,8 +85,14 @@ const Homepage = () => {
           </div>
         </div>
       </div>
-      {/* <div className="contact">
-      </div> */}
+      <div id="contact" className="contact">
+        <form id="contact-form" className="contact-content" action="https://formspree.io/f/mqknqqzb" method="post">
+          <input className="form-file" type="text" name="name" placeholder="Full Name" required />
+          <input className="form-file" type="email" name="mail" placeholder="Email Address" required />
+          <textarea name="message" placeholder="Enter Text Here" maxLength="500" required />
+          <input id="contact-btn" className="orange-btn" type="submit" value="Get in touch" />
+        </form>
+      </div>
     </main>
   );
 
