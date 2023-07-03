1a0944e372275d49323b4ab263ce99e6fefee1d3
J. Luis Arredondo <retky@thefoxsign.com>
2023-07-03 17:18:30 -0600

    Mod 'Projects' separated section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,14 @@
 import Landing from './Landing';
 import About from './About';
 import Contact from './Contact';
+import Projects from './Projects';
 import '../styles/HomePage.scss';
 
 const Homepage = () => {
   const view = (
     <main className="home">
       <Landing />
-      {/* <div className="projects">
-        <div className="projects-content">
-        </div>
-      </div> */}
+      <Projects />
       <About />
       <Contact />
     </main>
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,15 @@
+import '../styles/Projects.scss';
+
+const Projects = () => {
+  const view = (
+    <div id="projects" className="projects">
+      <div className="projects-content">
+        a
+      </div>
+    </div>
+  );
+
+  return view;
+};
+
+export default Projects;
