96d32967178f9d03d4a6edc7d1c599c70a9eb8ad
J. Luis Arredondo <retky@thefoxsign.com>
2023-07-03 16:36:02 -0600

    Mod 'landing section' separate component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,37 +1,13 @@
-import SocialLinks from '../components/SocialLinks';
+import Landing from './Landing';
 import '../styles/HomePage.scss';
 
 const Homepage = () => {
   const view = (
     <main className="home">
-      <div className="landing">
-        <div className="landing-content">
-          <div className="presentation">
-            <h1>
-              Hello,
-              <br />
-              <br />
-              I&apos;m Luis Arredondo
-              <br />
-              <span className="subtitle">
-                {'Full-Stack Web & '}
-                <span className="nowrap">Video Game</span>
-                {' Developer.'}
-              </span>
-            </h1>
-            <p>
-              Are you looking to create captivating experiences and solve complex
-              problems through the power of code and 3D?
-              <br />
-              I can help you with that.
-            </p>
-          </div>
-          <SocialLinks
-            section="landing"
-          />
-        </div>
-      </div>
+      <Landing />
       {/* <div className="projects">
+        <div className="projects-content">
+        </div>
       </div> */}
       <div id="about">
         <div className="about">
