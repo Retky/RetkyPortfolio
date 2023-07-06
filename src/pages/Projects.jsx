21eee0902638386b2ac9cf5bc863700981a5ad1b
J. Luis Arredondo <retky@thefoxsign.com>
2023-07-06 15:57:42 -0600

    Add 'projects section' basic structure

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProjectsData } from '../redux/projects';
 import '../styles/Projects.scss';
 
 const Projects = () => {
+  const [category, setCategory] = useState('development');
   const dispatch = useDispatch();
   const projectsList = useSelector((store) => store.projects);
 
@@ -11,15 +12,44 @@ const Projects = () => {
     dispatch(fetchProjectsData());
   }, [dispatch]);
 
+  const handleClickCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const section1 = (
+    <div className="test1">
+      {projectsList.map((project) => (
+        <div className="project-card" key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      ))}
+    </div>
+  );
+
+  const section2 = (
+    <div className="test2">
+      B
+    </div>
+  );
+
+  const section3 = (
+    <div className="test3">
+      C
+    </div>
+  );
+
   const view = (
     <div id="projects" className="projects">
       <div className="projects-content">
-        <div className="test">
-          {projectsList.map((project) => (
-            <div className="project-card" key={project.id}>
-              <h3>{project.name}</h3>
-            </div>
-          ))}
+        <div className="menu">
+          <button type="button" value="development" onClick={handleClickCategory}>Development</button>
+          <button type="button" value="videoGames" onClick={handleClickCategory}>Video Games</button>
+          <button type="button" value="3d" onClick={handleClickCategory}>3D Design</button>
+        </div>
+        <div className="content">
+          {category === 'development' && section1}
+          {category === 'videoGames' && section2}
+          {category === '3d' && section3}
         </div>
       </div>
     </div>
