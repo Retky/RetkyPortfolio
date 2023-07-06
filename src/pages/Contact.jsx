987a0edc617e7c7f9058623272b61abb0a7f0cb4
J. Luis Arredondo <retky@thefoxsign.com>
2023-07-06 16:18:07 -0600

    Mod 'contact' desktop style

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,13 +3,15 @@ import '../styles/Contact.scss';
 const Contact = () => {
   const view = (
     <div id="contact" className="contact">
-      <form id="contact-form" className="contact-content" action="https://formspree.io/f/mqknqqzb" method="post">
-        <h2>Reach Out and Let&apos;s Create Something Amazing!</h2>
-        <input className="form-file" type="text" name="name" placeholder="Full Name" required />
-        <input className="form-file" type="email" name="mail" placeholder="Email Address" required />
-        <textarea className="form-msg" name="message" placeholder="Enter Text Here" maxLength="1200" required />
-        <input id="contact-btn" className="orange-btn" type="submit" value="Get in touch" />
-      </form>
+      <div className="contact-content">
+        <h2 className="contact-title">Reach Out and Let&apos;s Create Something Amazing!</h2>
+        <form id="contact-form" className="contact-form" action="https://formspree.io/f/mqknqqzb" method="post">
+          <input className="form-file" type="text" name="name" placeholder="Full Name" required />
+          <input className="form-file" type="email" name="mail" placeholder="Email Address" required />
+          <textarea className="form-msg" name="message" placeholder="Enter Text Here" maxLength="1200" required />
+          <input id="contact-btn" className="orange-btn" type="submit" value="Get in touch" />
+        </form>
+      </div>
     </div>
   );
 
