426af9bfd2b9e7979776e0d48ef055bd97de1aed
J. Luis Arredondo <retky@thefoxsign.com>
2023-07-07 12:58:18 -0600

    Add 'projects menu' base style

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -42,9 +42,9 @@ const Projects = () => {
     <div id="projects" className="projects">
       <div className="projects-content">
         <div className="menu">
-          <button type="button" value="development" onClick={handleClickCategory}>Development</button>
-          <button type="button" value="videoGames" onClick={handleClickCategory}>Video Games</button>
-          <button type="button" value="3d" onClick={handleClickCategory}>3D Design</button>
+          <button className={`menu-btn ${category === 'development' ? 'active' : ''}`} type="button" value="development" onClick={handleClickCategory}>Development</button>
+          <button className={`menu-btn ${category === 'videoGames' ? 'active' : ''}`} type="button" value="videoGames" onClick={handleClickCategory}>Video Games</button>
+          <button className={`menu-btn ${category === '3d' ? 'active' : ''}`} type="button" value="3d" onClick={handleClickCategory}>3D Design</button>
         </div>
         <div className="content">
           {category === 'development' && section1}
