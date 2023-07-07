b5fdc8dd687d52795c375058385497a953ad6a09
J. Luis Arredondo <retky@thefoxsign.com>
2023-07-07 13:16:59 -0600

    Add 'projects cards'

--- src/pages/develop.jsx
+++ src/pages/develop.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProjectsData } from '../redux/projects';
+import ProjectCard from '../components/ProjectCard';
+import ProjectDetail from '../components/ProjectDetail';
 
 const development = () => {
   const dispatch = useDispatch();
@@ -13,9 +15,15 @@ const development = () => {
   const view = (
     <div className="test1">
       {projectsList.map((project) => (
-        <div className="project-card" key={project.id}>
-          <h3>{project.name}</h3>
-        </div>
+        <ProjectCard
+          key={project.id}
+          id={project.id}
+          name={project.name}
+          desc={project.desc}
+          topics={project.topics}
+          image={project.image}
+          seeWorkHandler={() => {}}
+        />
       ))}
     </div>
   );
